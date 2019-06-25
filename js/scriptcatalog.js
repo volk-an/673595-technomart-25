18fb197bfe4d7b22b9256c04ca73cf651d7bd785
volk-an <volk-an@mail.ru>
2019-06-25 18:52:17 +0700

    защита

--- js/scriptcatalog.js
+++ js/scriptcatalog.js
@@ -1,13 +1,15 @@
-var addToCart = document.querySelector(".add-to-cart");
+var addToCart = document.querySelectorAll(".add-to-cart");
 var infoPopup = document.querySelector(".info-popup");
 var closeInfo = document.querySelector(".info-button-close");
 
 
-addToCart.addEventListener("click", function (evt) {
+addToCart.forEach(function(button) {
+  button.addEventListener("click", function (evt) {
   evt.preventDefault ();
   console.log("клик");
   infoPopup.classList.add("modal-show");
-});
+  });
+})
 closeInfo.addEventListener("click", function (evt) {
   evt.preventDefault ();
   infoPopup.classList.remove("modal-show");
